bacb13590c40ca6edc31d57c1575da4c49ff0fcb
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-06-03 16:29:23 +0200

    add redux to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
 import './App.css';
 import { AppRouter } from './routes/AppRouter';
 
 function App() {
-  return <AppRouter />;
+  return (
+    <Provider store={configureStore()}>
+      <AppRouter />
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { combineReducers, applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { productsReducer } from './reducers/product.reducer';
+
+export default function configureStore(preLoadState) {
+  const rootReducer = combineReducers({
+    products: productsReducer
+  });
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(rootReducer, preLoadState, composeEnhancers(applyMiddleware(thunk)));
+}
