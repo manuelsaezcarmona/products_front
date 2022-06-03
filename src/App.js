6dba8ed6efe96287a576fd62513ec058d52ede18
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-06-03 12:20:14 +0200

    add addProduct to services/api.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
-import { getProductsBySearch } from './services/api';
+import { addProduct } from './services/api';
 
 async function App() {
-  const data = await getProductsBySearch('sin');
+  const data = await addProduct(
+    'loco',
+    'algo loco',
+    'http://algoloco.hpegs',
+    false,
+    12.99,
+    'buhardilla'
+  );
   console.log(data);
   return (
     <div className="App">
--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,6 @@
 
 // localhost:8000/api/products/search?term=sin -- Query
 
-// localhost:8000/api/products/  -POST
-
 export const getAllProducts = async () => {
   // localhost:8000/api/products/  - GETALL
   const baseURL = process.env.REACT_APP_API_URL;
@@ -26,6 +24,35 @@ export const getProductsBySearch = async (term) => {
     const resp = await fetch(`${baseURL}/${collection}/${resource}=${term}`);
     const data = await resp.json();
     return data;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const addProduct = async (
+  productName,
+  description,
+  imageURL,
+  isFavourite,
+  price,
+  section
+) => {
+  const product = { productName, description, imageURL, isFavourite, price, section };
+
+  // localhost:8000/api/products/  -POST
+  const baseURL = process.env.REACT_APP_API_URL;
+  const collection = 'api/products';
+
+  try {
+    const resp = await fetch(`${baseURL}/${collection}/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(product)
+    });
+    const result = resp.json();
+    return result;
   } catch (error) {
     return error.message;
   }
