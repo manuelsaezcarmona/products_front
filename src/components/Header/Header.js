7a6ae3175345703c66be804c6cbf7cc54dea74f8
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-06-04 09:38:41 +0200

    define breakpoints

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="header-container">
+      <header className="app-header">
+        <h1>Header</h1>
+      </header>
     </div>
   );
 }
