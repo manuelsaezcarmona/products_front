d8e42ebc86f1aeea79d6de53fffad469b8f456ec
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-06-08 18:10:10 +0200

    conditional rendering in search products

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { CloseOutlined } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
 import styles from './styles.module.scss';
 import { selectResultsSearch } from '../../redux/selectors';
+import { startGetAllProducts } from '../../redux/actions/products.action';
 
 export default function Results() {
   const { itemsCount, searchTerm } = selectResultsSearch();
+  const dispatch = useDispatch();
+
+  const isResults = itemsCount > 0;
 
   const handleCloseClick = () => {
     console.log('Cierra la ventana');
+    dispatch(startGetAllProducts());
   };
 
   return (
     <div className={styles.results}>
       <p className={styles.results__content}>{itemsCount} Items found</p>
-      <h2 className={styles.results__title}>Search Results</h2>
-      <h2 className={styles.results__title}>for {searchTerm}</h2>
+      {isResults ? (
+        <>
+          <h2 className={styles.results__title}>Search Results</h2>
+          <h2 className={styles.results__title}>for {searchTerm}</h2>
+        </>
+      ) : (
+        <h2 className={styles.results__title}>
+          No results found for {searchTerm}
+        </h2>
+      )}
+
       <CloseOutlined
         className={styles.results__closeIcon}
         onClick={handleCloseClick}
