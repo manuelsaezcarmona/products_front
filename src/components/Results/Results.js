6e10be6854b9d80374c256dcb3d63c48277246df
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-06-08 17:52:26 +0200

    add handles into Results component

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { CloseOutlined } from '@ant-design/icons';
 import styles from './styles.module.scss';
-import { selectProductsByFilter } from '../../redux/selectors';
+import { selectResultsSearch } from '../../redux/selectors';
 
 export default function Results() {
+  const { itemsCount, searchTerm } = selectResultsSearch();
+
+  const handleCloseClick = () => {
+    console.log('Cierra la ventana');
+  };
+
   return (
     <div className={styles.results}>
-      <p className={styles.results__content}>
-        {' '}
-        {selectProductsByFilter().length} Items found
-      </p>
+      <p className={styles.results__content}>{itemsCount} Items found</p>
       <h2 className={styles.results__title}>Search Results</h2>
-      <h2 className={styles.results__title}>for -Bathroom taps-</h2>
-      <CloseOutlined className={styles.results__closeIcon} />
+      <h2 className={styles.results__title}>for {searchTerm}</h2>
+      <CloseOutlined
+        className={styles.results__closeIcon}
+        onClick={handleCloseClick}
+      />
     </div>
   );
 }
