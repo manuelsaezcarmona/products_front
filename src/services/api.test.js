0cdbf310dd2724f072d5b94e209ae319600151f7
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-06-09 15:11:17 +0200

    add test to gelAllProducts function in api.test.js

---
+++ src/services/api.test.js
@@ -0,0 +1,26 @@
+import { getAllProducts } from './api';
+
+describe('given the getAllProducts function', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  test('when the promise Resolves , expect some data', async () => {
+    const fetchMock = jest
+      .spyOn(global, 'fetch')
+      .mockImplementation(() =>
+        Promise.resolve({ json: () => Promise.resolve([]) })
+      );
+    const res = await getAllProducts();
+
+    expect(fetchMock).toHaveBeenCalledWith(
+      'http://localhost:8000/api/products/'
+    );
+
+    expect(res).toEqual([]);
+  });
+  test('when the promise Reject , expect some data', async () => {
+    jest.spyOn(global, 'fetch').mockRejectedValue('testValue');
+    const res = await getAllProducts();
+    expect(res).toBe('testValue');
+  });
+});
